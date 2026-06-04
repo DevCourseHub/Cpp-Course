84695af3ed28e4e9a123c5a8c9a5ba33901569fa
gitprocoder <githubforprocoders@gmail.com>
2026-06-04 12:54:58 +0530

    cpp

--- 1_Basic.cpp
+++ 1_Basic.cpp
@@ -5,4 +5,5 @@ int main (){
     cout<<"Hello World!";//It is used to print words.<< is called insertion operator
     system("pause"); //we put this line to tell the compiler that the program doesnt exit until i press any key.It helps us to run the .exe file directly.Without this the .exe exits less than 1 sec and we dont see output.
     return 0; //We use return 0; to tell the cpp compiler that the main function has ended here.After C++ 11 version if we dont put return 0 at the end it automatically put there when running the program.
+    //when it is compiled it creates an aplication .exe we can run it .
 } 
